ccc8ff354ea8a8bdd7ae06cb2114e027c36938a5
diegoG82 <diegogiardina@hotmail.it>
2023-05-06 10:06:08 +0200

    milestone 2 done

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-if (isset($_GET['length'])){
-    $length =$_GET['length'];
-
-    $characters ='abcdefghilmnopqrstuvzABCD1234567890&%$£§ç@';
-    $password ='';
-    for ($i = 0; $i <$length; $i++){
-        $password .=$characters[random_int(0, strlen($characters) - 1)];
-    }
-    echo "your password is: $password";
+include 'functions.php';
+if (isset($_GET['length'])) {
+    $length = $_GET['length'];
+    $password = random_psw($length);
+    // $characters ='abcdefghilmnopqrstuvzABCD1234567890&%$£§ç@';
+    // $password ='';
+    // for ($i = 0; $i <$length; $i++){
+    //     $password .=$characters[random_int(0, strlen($characters) - 1)];
+    // }
+    // echo "your password is: $password";
 }
 
 ?>
@@ -15,22 +16,42 @@ if (isset($_GET['length'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Password</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
 
-<form action="index.php" method="GET">
-  <label for="length">Lunghezza password:</label>
-  <input type="number" id="length" name="length" min="5" max="15" required>
-  <button type="submit">Create password</button>
-</form>
+    <h1 class="text-center mt-4">
+        NOT REALLY SAFE PASSWORD GENERATOR
+    </h1>
+
+    <div class="container text-center">
+        <form action="index.php" method="GET">
+            <label for="length">Lunghezza password:</label>
+            <input type="number" id="length" name="length" min="5" max="15" required>
+            <button type="submit">Create password</button>
+        </form>
+
+        <p class mt-4>La tua password e' <?php echo $password ?></p>
 
 
 
-    
+
+
+    </div>
+
+
+
+
+
+
 </body>
+
 </html>
